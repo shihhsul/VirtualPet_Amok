0c45da6eac22f07c9ec4140ece2b94a4c4a0a116
shihhsul <wrtyujmoo@gmail.com>
2023-07-23 16:43:06 -0400

    added a stupid testing

--- src/test/java/pets_amok/VirtualPetTest.java
+++ src/test/java/pets_amok/VirtualPetTest.java
@@ -1,4 +1,9 @@
 package pets_amok;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 public class VirtualPetTest {
+    @Test
+    void appHasAGreeting() {
+        assertTrue(true, "app should have a greeting");
+    }
 }
